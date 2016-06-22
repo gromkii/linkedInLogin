1b5f78198649310d125c7e0bc71b5109f01698a4
Dax <limeside@gmail.com>
2016-06-22 11:29:54 -0500

    Edit auth routes and strategy for localhost. :rabbit:

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var cookieSession = require('cookie-session');
+var LinkedInStrategy = require('passport-linkedin').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -24,6 +27,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env['SECRET_KEY']]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  //later this will be where you selectively send to the browser an identifier for your user, like their primary key from the database, or their ID from linkedin
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  //here is where you will go to the database and get the user each time from it's id, after you set up your db
+  done(null, obj);
+});
+
+passport.use(new LinkedInStrategy({
+    consumerKey: process.env['LINKEDIN_API_KEY'],
+    consumerSecret: process.env['LINKEDIN_SECRET_KEY'],
+    callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback",
+    scope: ['r_emailaddress', 'r_basicprofile'],
+  },
+  function(token, tokenSecret, profile, done) {
+
+      // To keep the example simple, the user's LinkedIn profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the LinkedIn account with a user record in your database,
+      // and return that user instead (so perform a knex query here later.)
+      return done(null, profile);
+}));
 
 app.use('/', routes);
 app.use('/users', users);
